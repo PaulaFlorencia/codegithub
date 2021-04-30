1c79ad6166d2617e7d25f6160d042e095535dace
Paula Florencia González Troncoso <shimbalaie@MacBook-Pro-de-Paula.local>
2021-04-30 17:27:22 +0200

    changement sapply par outer

--- p.R
+++ p.R
@@ -16,8 +16,27 @@ Z = rgev(size, loc = mu, scale = 1, shape = 0)
 theta_theo <- 1 / exp(mu)
 p12_theo <- 1 / (1 + theta_theo)
 
-#############################################################################
+##########################################################################
+# Example 2
+# Frechet
+# Simulations
+size <-  250 * 4
+rp <- 50
+t <- seq.int(size)/size
+sigma <- seq(1, 2, length.out = size)
+xi = 0.5
+x = rgev(size * 1/4, loc = 1, scale = xi, shape = xi)
+z = rgev(size, loc = sigma, scale = xi * sigma, shape = xi)
+theta_theo <- sigma^(-1 / xi)
+p12_theo <- 1 / (1 + theta_theo)
+G_theo <- function(z) pgev(z, loc = 1, scale = xi, shape = xi)
+far_theo_rp <- (1 - theta_theo) * (1 - 1/rp)
 
+#############################################################################
+# Epanechnikov density distribution
+depan <- function(x){d <- (3 / 4) * (1 - x^2)}
+
+##########################################
 p12_NonPar <- function(X,Z,t,h,m=512, k=dnorm){
   
   # Compute empirical cdf of NAT
@@ -30,7 +49,7 @@ p12_NonPar <- function(X,Z,t,h,m=512, k=dnorm){
   kt <- numeric(m)
   
   # Numerator Kt
-  const <- sqrt(2 * pi) * h * length(x)
+  const <- sqrt(2 * pi) * h * length(t)
   Kt <- sapply(t, function(tj) k((t_eval - tj)/h))
   
   # Ponderation
