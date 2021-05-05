4d09f67ff7f70c9ec5137ca5ea73d7b8c2228b5d
PaulaFlorencia <pfgtroncoso@gmail.com>
2021-05-05 11:41:54 +0200

    CI computation and plot

--- p.R
+++ p.R
@@ -29,7 +29,8 @@ theta_theo <- 1 / exp(mu)
 p12_theo <- 1 / (1 + theta_theo)
 
 ############ Example 2
-# Frechet - (set.seed(304))
+# Frechet - 
+set.seed(304)
 # Simulations
 size <-  250 * 4
 rp <- 50
@@ -42,6 +43,9 @@ theta_theo <- sigma^(-1 / xi)
 p12_theo <- 1 / (1 + theta_theo)
 G_theo <- function(z) pgev(z, loc = 1, scale = xi, shape = xi)
 far_theo_rp <- (1 - theta_theo) * (1 - 1/rp)
+
+######### R.V from a random distribution 
+
 
 #########################  Kernels exemples ##########################################
 # Epanechnikov density distribution
@@ -67,21 +71,47 @@ p12_NonPar <- function(X,Z,t,t_eval,h,kern= dEpan){
 }
 
 # plot p12_theo & p12_hat
-p12_hat <- p12_NonPar(X_gev,Z_gev_trend,t,t,0.2,)
+p12_hat <- p12_NonPar(x,z,t,t,0.2,)
 plot(t,p12_theo, type = "l", lwd = 4, xlab = "t", ylab = "p12")
 lines(t,p12_hat, col = "red", lwd = 2)
 
 
 ######################### Computation of IC ###################################
 
-# Computation of variance
-G_emp <- ecdf(X)
-G_Z <- G_emp(Z)
-
-VarA <- size # or just size
 t <- seq.int(size)/size
 t_eval <- seq.int(size)/size
 
+G_emp <- ecdf(X)
+G_Z <- G_emp(Z)
+
+######## Var(A) and Var(B):
+
+dEpan_2<- function(x){
+  tt <- ((3/4)* (1-x^2))^2
+  tt[-1>x] <- 0
+  tt[x>1] <- 0
+  return (tt)
+}
+# integral of squared kernel
+K22<-integrate(dEpan_2,-1,1)$value
+
+kern_dens <- function(t,teval,h,kern=dEpan) {
+  fh <- numeric(length(t_eval))
+  for (i in seq_along(t))
+    for (j in seq_along(t_eval))
+      fh[i] <- fh[i] + kern((t_eval[i] - t[j])/h) 
+    fh <- fh / ( h * length(t))
+    return(fh)
+}
+# density distribution of t 
+f_t<-kern_dens(t,t,0.2)
+# variance of t 
+sigma_t <- var(t)
+
+# Var(A):
+VarA <- (sigma_t * K22 * f_t )^0.5
+
+# Var(B):
 Khj <- outer(t_eval, t, function(zz,z) dEpan((zz - z) / 0.2))
 Khi <- outer(t_eval, t, function(zz,z) dEpan((zz - z) / 0.2))
 E <- mean(outer(G_Z,G_Z, pmin) - outer(G_Z,G_Z,"*"))
@@ -98,14 +128,13 @@ error <- qnorm(0.975)*s/sqrt(size)
 left <- p12_hat-error
 right <- p12_hat+error
 
-left_ex <-p12_hat-0.05 #test pour voir si le plot marche bien
-right_ex<-p12_hat+0.05
-
 ##### plot p12_hat with CI
 library(ggplot2)
-df <-data.frame(x=t,y=p12_hat,z1=left, z2=right)
-p12plot<-ggplot(df,aes(x=t,y=p12_hat)) + geom_line()
-p12plot <- p12plot + geom_ribbon(aes(ymin=left_ex, ymax=right_ex), linetype=2, alpha=0.1)
+df <-data.frame(x=t,y=p12_hat,z1=left, z2=right,theo=p12_theo)
+p12plot<-ggplot(df,aes(x=t,y=p12_hat)) + geom_line(colour="red") 
+p12plot<- p12plot + geom_line(aes(y=p12_theo), colour="black")
+p12plot <- p12plot + geom_ribbon(aes(ymin=left, ymax=right), linetype=2, alpha=0.1) 
+p12plot <- p12plot + ggtitle("p12 evolution over time") + ylab("p12") + xlab("time")
 p12plot
 
 dev.off()
