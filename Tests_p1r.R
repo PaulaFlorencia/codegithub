4763accc2149e5e5e7a7ae35b3401b45b130b020
PaulaFlorencia <pfgtroncoso@gmail.com>
2021-06-16 08:37:51 +0200

    W-class non-stationary simulation and visualisation of distributions in time

--- Tests_p1r.R
+++ Tests_p1r.R
@@ -9,10 +9,10 @@ phat <-c(1/2, 1/3)
 phat <-c(0.86, 0.79) 
 fg <- function(theta){functiong(theta,vecx=phat)}
 
-lambda <- seq(0.01, 2, 0.01)
-k <- seq(0.01, 2, 0.01)
-p12_mat <- matrix(NA, nrow = length(lambda), ncol = length(k))
-p13_mat <- matrix(NA, nrow = length(lambda), ncol = length(k))
+lambda <- seq(0.01, 1.5, 0.01)
+k <- seq(0.01, 1.5, 0.01)
+p12_mat <- matrix(nrow = length(lambda), ncol = length(k))
+p13_mat <- matrix(nrow = length(lambda), ncol = length(k))
 for(i in seq_along(lambda)){
   for(j in seq_along(k)){
     p12_mat[i, j] <- fg(c(lambda[i], k[j]))[1]
@@ -25,7 +25,8 @@ image.plot(lambda, k, p12_mat)
 points(lambda[idx12[1]], k[idx12[2]], col="white", pch=20)
 image.plot(lambda, k, p13_mat)
 points(lambda[idx13[1]], k[idx13[2]], col="white", pch=20)
-
+#39 186
+# 53  29
 ############### (A) fsolve() ###############################3
 library(pracma)
 
@@ -44,8 +45,8 @@ EGMMweibul3 <- fsolve3(fg,x0=startval_t)
 
 
 # A.1 Computation of lambda_t, k_t for each time. X,Z stationary
-data <- simulWclass(m=20,n=20,N=2,ksiX=-0.20,ksiZ=-0.25,sigX=1,sigZ=1)
-tt <- seq.int(20)/20
+data <- simulWclass(m=100,n=100,N=2,ksiX=-0.20,ksiZ=-0.25,sigX=1,sigZ=1)
+tt <- seq.int(100)/100
 matp<-GZestimation(data$matX,data$matZ,tt,tt,11,kern=dEpan) 
 theta<-weibullGMMestim(matp$matp12,matp$matp13,truevalues=NULL)
 theta3<-weibullGMMestim3(matp$matp12,matp$matp13,truevalues=NULL) # ... lam= 336.1192 ,k= -2826.279 error...
@@ -73,47 +74,54 @@ EOptimweibull <- optim(c(0.7, 1.3),fg1,method="BFGS",vecx=c(0.86, 0.79))
 ### Using methos L-BFGS-B with bound (no negative parameters)
 EOptimweibull <- optim(c(0.7, 1.3),fg1,method="L-BFGS-B",vecx=c(0.86, 0.79),lower = 0.001)
 # lam= 0.08297448 ,k= 0.4102397 .  convergence
-
+EOptimweibull <- optim(c(0.5, 0.8),fg1,method="L-BFGS-B",vecx=c(0.86, 0.79),lower = 0.001)
 
 # B.1
 ## computation of lambda_t, k_t for each time. X,Z stationary 
 
 data <- simulWclass(m=20,n=20,N=2,ksiX=-0.20,ksiZ=-0.25,sigX=1,sigZ=1)
 tt <- seq.int(20)/20
-matp<-GZestimation(data$matX,data$matZ,tt,tt,11,kern=dEpan) 
+matp<-GZestimation(data$matX,data$matZ,tt,tt,0.1,kern=dEpan) 
+plot(tt,matp$matp13[,1],xlab="t",ylab="p12_hat")
+
 thetahat<-weibullGMMestim4 (matp$matp12,matp$matp13,truevalues=NULL)
+plot(tt,thetahat$lambdahat[,1], xlab="t",ylab="lambda_hat") # parameter change a llitle bit
+plot(tt,thetahat$khat[,1],xlab="t",ylab="k_hat")
+
 # p1.10.t for each trayectory
 p1rfarestim <- p1rfarW(thetahat$lambdahat,thetahat$khat,matrix(10,ncol=2,nrow=20)) 
 rvalues <- seq(5,30,by=5)
 p1rfarchoix <- p1rfarW(rep(thetahat$lambdahat[1],6),rep(thetahat$khat[1],6),rvalues)
 
+
 # B.2
 ## computation of lambda_t, k_t for each time. Z_t non stationary and same support
 
-size=1000
+size=20
 tt <- seq.int(size)/size
 mu = 2 + seq(0, 5, length.out = size) 
 X = rgev(size * 5, loc = 0, scale = 1, shape = 0)
 Z = rgev(size, loc = mu, scale = 1, shape = 0)
 matp<-GZestimation(X,Z,tt,tt,0.11,kern=dEpan)
+#plot(tt,matp$matp13,type="l", col="blue")
+#lines(tt,matp$matp12,type="l")
 thetahat<-weibullGMMestim4 (matp$matp12,matp$matp13,truevalues=NULL)
 rvalues <- seq(5,30,by=5)
-
 p1rfar1<-p1rfarW(rep(thetahat$lambdahat[1],6),rep(thetahat$khat[1],6),rvalues) # t1 and sequence of r
-p1rfarestim <- p1rfarW(thetahat$lambdahat,thetahat$khat,matrix(10,ncol=2,nrow=1000))
+p1rfarestim <- p1rfarW(thetahat$lambdahat,thetahat$khat,matrix(10,ncol=2,nrow=20))
 # r=10 for all t.vec. Here we can see that here we are taking in consideration time
 
 p1rfar<-p1rfarW_temps(thetahat[[1]],thetahat[[2]],matrix(4,ncol=1,nrow=size))
-plot(p1rfar$p1r)
-lines(matp$matp12)
-lines(matp$matp13)
+plot(tt,p1rfar$p1r,type="l",col="blue",xlab="t",ylab="p12_4")
+lines(tt,matp$matp12,col="gray")
+lines(tt,matp$matp13,col="gray")
 
 #########
 
 ################ testing the method with "known" W-class F and CF #############
 
 # 1. for 1 trayactory {(X)t,(Z)t} (N=1)
-size <-  100
+size <-  200
 tt <- seq.int(size)/size
 muz = 2 + seq(0, 5, length.out = size)
 mux = 0
@@ -166,47 +174,21 @@ lines(tt,p1rfar$p1r)
 # 2. for multiple trajectories {(X)t,(Z)t}
 
 # Both Gumbel and linear trend un muz. We know two Gumbel have the same support
-FastTestforp1r_gev <- function(tt,h,r,m,n,N,sigX.vec,sigZ.vec,muX.vec,muZ.vec){
-  
-  theta_theo <- 1 / exp(muZ.vec)
-  p1r_t_theo <- 1 / (1 + (r-1)*theta_theo)
-  
-  matX <- matrix(nrow=m,ncol=N)
-  matZ <- matrix(nrow=n,ncol=N)
-  matp <- matrix(nrow=n,ncol=N)
-  
-  for (j in 1:N){
-    matX[,j] <- rgev(m, loc = muX.vec, scale = sigX.vec, shape = 0)
-    matZ[,j] <- rgev(n, loc = muZ.vec, scale = sigZ.vec, shape = 0)
-  }
-  matp<-GZestimation(matX,matZ,tt,tt,h,kern=dEpan)
-  
-  thetahat<-weibullGMMestim4 (matp$matp12,matp$matp13,truevalues=NULL)
-  p1rfar<-p1rfarW_temps(thetahat[[1]],thetahat[[2]],matrix(r,ncol=N,nrow=n))
-  p1r_mean <- rowMeans(p1rfar$p1r)
-  
-  plot(tt,p1r_t_theo,type="l",col="red",xlab="time",ylab="p1r_t", main=paste("evolution over time of p1r with r=",r))
-  lines(tt,p1r_mean)
-  for (i in 1:N){
-    lines(tt,p1rfar$p1r[,i],col="gray")
-  }
-  return(list("matp1r"=p1rfar$p1r,"p1r_mean"=p1r_mean, "p1rmean"=p1r_mean))
-}
 
-size <-  20
+size <-  50
 tt <- seq.int(size)/size
 muz = 2 + seq(0, 5, length.out = size)
 mux = 0
 sigmax = 1
 sigmaz = 1
 testmoyen<-FastTestforp1r_gumbel(tt,0.11,5,20,20,10,rep(sigmax,size),rep(sigmaz,size),rep(mux,size),muz)
-testmoyen<-FastTestforp1r_gumbel(tt,0.11,5,20*2,20,10,rep(sigmax,size*2),rep(sigmaz,size),rep(mux,size*2),muz)
+testmoyen<-FastTestforp1r_gumbel(tt,0.11,5,50*2,50,16,rep(sigmax,size*2),rep(sigmaz,size),rep(mux,size*2),muz)
 
-plot(tt,testmoyen$matp1r[,1], type="l")
+plot(tt,testmoyen$matp1r[,1], type="l") # 1 trayectory
 
 # 3. Fonction similar two (2.) but for a type of Frechet, in this case the trajectories are not necessay W-class
 
-
+############# not ready :
 size <-  20
 rp <- 50
 tt <- seq.int(size)/size
@@ -219,11 +201,16 @@ p12_theo <- 1 / (1 + theta_theo)
 G_theo <- function(z) pgev(z, loc = 1, scale = xi, shape = xi)
 far_theo_rp <- (1 - theta_theo) * (1 - 1/rp)
 
-testmoyen<-FastTestforp1r_gumbel(tt,0.5,5,size*2,size,10,rep(xi,size*2),sigma,rep(1,size*2),sigma)
 
-FastTestforp1r_frechet <- function(tt,h,r,m,n,N,xi.vec,sigma.vec,muX.vec,muZ.vec){
+size <-  20
+tt <- seq.int(size)/size
+sigma <- seq(1, 2, length.out = size)
+xi = 0.5
+testmoyen<-FastTestforp1r_frechet(tt,0.5,5,size*2,size,10,xi,sigma,rep(1,size*2),sigma)
+
+FastTestforp1r_frechet <- function(tt,h,r,m,n,N,xi,sigma.vec,muX.vec,muZ.vec){
   
-  theta_theo <- sigma.vec^(-1 / xi.vec)
+  theta_theo <- sigma.vec^(-1 / rep(xi,n))
   p12_theo <- 1 / (1 + theta_theo)
   
   matX <- matrix(nrow=m,ncol=N)
@@ -231,8 +218,8 @@ FastTestforp1r_frechet <- function(tt,h,r,m,n,N,xi.vec,sigma.vec,muX.vec,muZ.vec
   matp <- matrix(nrow=n,ncol=N)
   
   for (j in 1:N){
-    matX[,j] <- rgev(m, loc = muX.vec, scale = xi.vec, shape = xi.vec)
-    matZ[,j] <- rgev(n, loc = muZ.vec, scale = xi.vec*muZ.vec, shape = xi.vec)
+    matX[,j] <- rgev(m, loc = muX.vec, scale = rep(xi,m), shape = xi)
+    matZ[,j] <- rgev(n, loc = sigma.vec, scale = rep(xi,n)*sigma.vec, shape =xi)
   }
   matp<-GZestimation(matX,matZ,tt,tt,h,kern=dEpan)
   
@@ -248,5 +235,31 @@ FastTestforp1r_frechet <- function(tt,h,r,m,n,N,xi.vec,sigma.vec,muX.vec,muZ.vec
   return(list("matp1r"=p1rfar$p1r,"matfar"=p1rfar$far,"p1rmean"=p1r_mean))
 }
 
+################ Simulation of W-class trajectories with F = non-stationary ###########3
+
+size=200
+tt <- seq.int(size)/size
+sigma <- seq(1, 2, length.out = size)
+
+simul<-simulWclass_nonStationnary(m=size*2,n=size,N=1,ksiX=0.2,ksiZ=0.25,sigX=0.7,muX=0,muZ=sigma,sigZ=0,unknownMu=FALSE) 
+# obs: we still need to add a the fonctionality for testing if all parameters given creater W-class trajectory or not
+# And also define warnings or errors in the focntion because here we are assuming the its goning to be "well used"
+
+G<-ecdf(simul$matX)
+plot(tt,G(simul$matZ))
 
 
+# plot in time
+# Fast visualisation of de evolution on the trajectories {(X)t,(Z)t}
+a<-length(simul$mu_Z)
+muZ_t= c(simul$mu_Z[1],simul$mu_Z[a/4],simul$m_Z[a/2], simul$mu_Z[3*a/4], simul$mu_Z[a])
+sigZ_t = c(sigma[1],sigma[a/4],sigma[a/2], sigma[3*a/4], sigma[a])
+plot(density(simul$matX),col="red")
+Z1 = rgev(size, loc = muZ_t[1], scale = sigZ_t[1], shape = 0.25)
+lines(density(Z1),col="gray")
+Z2 = rgev(size, loc = muZ_t[2], scale = sigZ_t[2], shape = 0.25)
+lines(density(Z2),col="green")
+Z3 = rgev(size, loc = muZ_t[3], scale = sigZ_t[3], shape = 0.25)
+lines(density(Z3),col="blue")
+Z4 = rgev(size, loc = muZ_t[4], scale = sigZ_t[4], shape = 0.25)
+lines(density(Z4),col="purple")
